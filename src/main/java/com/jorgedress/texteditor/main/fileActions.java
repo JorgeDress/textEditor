bc81fc8f5d6578c15624c0a2c8c27e69fc3e2e95
jorgedress <jdrgc07@gmail.com>
2020-07-19 22:54:24 +0200

    Beta 0.1.2: 	-Fixed String returning 'null' 	-Fixed plugin maven-jar-plugin warning

--- src/main/java/com/jorgedress/texteditor/main/fileActions.java
+++ src/main/java/com/jorgedress/texteditor/main/fileActions.java
@@ -12,40 +12,44 @@ import javax.swing.*;
  * @author jorge
  */
 public class fileActions extends com.jorgedress.texteditor.main.mainFrame {
-    
-    public static int fileNumber;
-    public String aatitle;
-    public static String atitle;
-    public static JTabbedPane newtbp;
-    
-        public fileActions() {
-            fileNumber = 0+tabPane.getTabCount();
-            aatitle = "HII";
-            atitle = aatitle.toString();
-            newtbp = new JTabbedPane();
-        }
-    
+
+    public static int filesOpened = 1;
+    //public static int fileNumber;
+    public static String atitle = "NewFile("+(filesOpened+1)+").txt";
+    //public static String atitle;
+    //public static JTabbedPane newtbp;
+
+    public fileActions() {
+        //fileNumber = 0 + tabPane.getTabCount();
+        //aatitle = "HII";
+        //atitle = aatitle;
+        //newtbp = new JTabbedPane();
+    }
+
     public static void newFile() {
         System.out.println(atitle);
-        tabPane.add(atitle, newtbp);
-        
+        //tabPane.add(atitle, newtbp);
+
     }
+
     public static void closeFile() {
-        
+
     }
+
     public static void saveFile() {
-        
+
     }
+
     public static void exit(boolean Saved) {
         if (!Saved) {
-            
+
             int jopResult = JOptionPane.showOptionDialog(null,
                     "There are not saved changes, if you close the file you will lose them."
                     + "Are you sure do you want to exit?",
                     "Warning",
                     JOptionPane.YES_NO_OPTION,
                     JOptionPane.YES_NO_OPTION, null, null, null);
-            
+
             switch (jopResult) {
                 case JOptionPane.YES_OPTION:
                     System.exit(0);
@@ -53,12 +57,13 @@ public class fileActions extends com.jorgedress.texteditor.main.mainFrame {
                 case JOptionPane.NO_OPTION:
                     JOptionPane.getRootFrame().dispose();
             }
-            
+
         } else {
             System.exit(0);
         }
     }
+
     public static void main(String[] args) {
-        
+
     }
 }
